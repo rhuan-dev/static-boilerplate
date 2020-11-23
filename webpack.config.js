4ebc0b9a662fc25b2d15fac43c0f12c8291f7d9d
Rhuan <rhcarlosweb@gmail.com>
2020-11-23 01:22:47 -0300

    :tada: start

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path    = require('path')
+const webpack = require('webpack')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+const config = {
+    entry  : {
+        app: "./src/assets/js/app.js",
+        obg: "./src/assets/js/obg.js"
+    },
+    output : {
+        filename: "./assets/js/[name].bundle.js",
+        path    : path.resolve(__dirname, "dist")
+    },
+    module : {
+        rules: [
+            {
+                test   : /\.ejs$/,
+                loader : 'ejs-loader',
+                options: {
+                    esModule: false
+                }
+            },
+            {
+                test  : /\.hbs$/,
+                loader: 'handlebars-loader',
+            },
+        ],
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host  : 'localhost',
+            port  : 3000,
+            server: {baseDir: ['dist']}
+        }),
+
+        new HtmlWebpackPlugin({
+            filename          : "index.html",
+            template          : "./src/views/pages/index.hbs",
+            templateParameters: require("./site-data.json"),
+            chunks            : ['app']
+        }),
+
+        new HtmlWebpackPlugin({
+            filename          : "obrigado-boleto.html",
+            template          : "./src/views/pages/obrigado-boleto.hbs",
+            templateParameters: require("./site-data.json"),
+            chunks            : ['obg']
+        }),
+
+        new HtmlWebpackPlugin({
+            filename          : "obrigado-cartao.html",
+            template          : "./src/views/pages/obrigado-cartao.hbs",
+            templateParameters: require("./site-data.json"),
+            chunks            : ['obg']
+        }),
+
+        new HtmlWebpackPlugin({
+            filename          : "termos-de-uso.html",
+            template          : "./src/views/pages/termos-de-uso.hbs",
+            templateParameters: require("./site-data.json"),
+            chunks            : ['obg']
+        }),
+
+        new HtmlWebpackPlugin({
+            filename          : "politica-de-privacidade.html",
+            template          : "./src/views/pages/politica-de-privacidade.hbs",
+            templateParameters: require("./site-data.json"),
+            chunks            : ['obg']
+        }),
+    ]
+}
+
+module.exports = config
