141bdf7c14788a290a44a1755cbd8553ed1548f8
Rhuan <rhcarlosweb@gmail.com>
2021-01-05 01:33:25 -0300

    :arrow_up: update dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -125,34 +125,6 @@ const config = {
             template          : "./src/views/pages/index.hbs",
             templateParameters: require("./site-data.json"),
             chunks            : ['app']
-        }),
-
-        new HtmlWebpackPlugin({
-            filename          : "obrigado-boleto.html",
-            template          : "./src/views/pages/obrigado-boleto.hbs",
-            templateParameters: require("./site-data.json"),
-            chunks            : ['obg']
-        }),
-
-        new HtmlWebpackPlugin({
-            filename          : "obrigado-cartao.html",
-            template          : "./src/views/pages/obrigado-cartao.hbs",
-            templateParameters: require("./site-data.json"),
-            chunks            : ['obg']
-        }),
-
-        new HtmlWebpackPlugin({
-            filename          : "termos-de-uso.html",
-            template          : "./src/views/pages/termos-de-uso.hbs",
-            templateParameters: require("./site-data.json"),
-            chunks            : ['obg']
-        }),
-
-        new HtmlWebpackPlugin({
-            filename          : "politica-de-privacidade.html",
-            template          : "./src/views/pages/politica-de-privacidade.hbs",
-            templateParameters: require("./site-data.json"),
-            chunks            : ['obg']
         })
     ]
 }
